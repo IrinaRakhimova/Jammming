495279d3a837a2dbd2777660f36aa97a8a1eafc5
irina <rakhimovairinav@gmail.com>
2024-05-08 23:01:58 +0300

    Callback changed

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 let accessToken;
 let userId;
 const clientId = "cc27cc8fdd314c4c8758dbc6d06c60c8";
-const redirectUrl = "http://localhost:3000/callback";
+const redirectUrl = "http://jammming-irina/callback";
 
 
 const spotify = {
